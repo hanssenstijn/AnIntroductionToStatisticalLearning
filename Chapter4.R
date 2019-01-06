377b50ae83949ff52675e81a07d257ae4e77c872
Stijn <srs.hanssen@student.maastrichtuniversity.nl>
2019-01-06 20:52:02 +0100

    update chapter 4 script
    Answers to the applied section
    - correlation plot
    - attach()
    - logistic regresion glm(...., family= bionomial)
    - predict(..., type = "response")
    - select training set
    -  LDA
    - QDA
    - logisitc regression
    - KNN
    - writing functions

--- Chapter4.R
+++ Chapter4.R
@@ -156,3 +156,176 @@ table(knn.pred ,test.Y)
 #knn = 3
 knn.pred=knn (train.X,test.X,train.Y,k=3)
 table(knn.pred ,test.Y)
+
+# Aplied chapter 4
+# remove all objects at once
+rm(list=ls())
+
+# Q10
+
+#library
+library(ISLR)
+library(corrplot)
+#data
+data("Weekly")
+
+# numerical/graphical summaries
+summary(Weekly)
+pairs(Weekly)
+cor(Weekly[,-9])
+M=cor(subset(Weekly, select=-Direction))
+corrplot(M,method = "circle", type="upper")
+
+attach(Weekly)
+glm.fit = glm(Direction ~ Lag1 + Lag2 + Lag3 + Lag4 + Lag5 + Volume, data = Weekly, 
+              family = binomial)
+summary(glm.fit)
+
+#predict
+glm.probs = predict(glm.fit, type = "response")
+# confusion matrix
+glm.pred = rep("Down", length(glm.probs))
+glm.pred[glm.probs > 0.5] = "Up"
+table(glm.pred, Direction)
+
+glm.fit = glm(Direction ~ Lag2, data = Weekly, 
+              family = binomial)
+summary(glm.fit)
+
+# predict on 2009 and 2010
+train = (Year < 2009)
+# save all rows which are true
+Weekly.0910 = Weekly[!train, ]
+# model using only Lag2
+glm.fit = glm(Direction ~ Lag2, data = Weekly, family = binomial, subset = train)
+# predict for new data
+glm.probs = predict(glm.fit, Weekly.0910, type = "response")
+# add collumn with "Down"
+glm.pred = rep("Down", length(glm.probs))
+# All which have higher prediction than 0.5 will get "Up" instead of "Down"
+glm.pred[glm.probs > 0.5] = "Up"
+# save all rows which are in training data
+Direction.0910 = Direction[!train]
+# confusion matrix
+table(glm.pred, Direction.0910)
+# fraction of correct predictions
+mean(glm.pred == Direction.0910)
+
+# library for lda
+library(MASS)
+lda.fit = lda(Direction ~ Lag2, data = Weekly, subset = train)
+lda.pred = predict(lda.fit, Weekly.0910)
+table(lda.pred$class, Direction.0910)
+mean(lda.pred$class == Direction.0910)
+
+# QDA
+qda.fit = qda(Direction ~ Lag2, data = Weekly, subset = train)
+qda.class = predict(qda.fit, Weekly.0910)$class
+table(qda.class, Direction.0910)
+mean(qda.class == Direction.0910)
+
+# library KNN
+library(class)
+# set training x
+train.X = as.matrix(Lag2[train])
+# set test x
+test.X = as.matrix(Lag2[!train])
+# set prediction y
+train.Direction = Direction[train]
+set.seed(1)
+knn.pred = knn(train.X, test.X, train.Direction, k = 1)
+table(knn.pred, Direction.0910)
+mean(knn.pred == Direction.0910)
+
+# remove all objects at once
+rm(list=ls())
+
+# Q11
+
+#library
+library(ISLR)
+library(corrplot)
+#data
+data("Auto")
+
+# summary
+summary(Auto)
+
+attach(Auto)
+# create binary variable "mpg01" 0 is lower than median, 1 if its higher
+mpg01 = rep(0, length(mpg))
+mpg01[mpg > median(mpg)] = 1
+# combine the new variable to the auto dataset
+Auto = data.frame(Auto, mpg01)
+
+cor(Auto[, -9])
+M=cor(subset(Auto, select=-name))
+corrplot(M,method = "circle", type="upper")
+# doesn't work well since mpg01 is 0 or 1
+pairs(Auto)
+
+# split data
+# if the year is even
+train = (year%%2 == 0) 
+test = !train
+Auto.train = Auto[train, ]
+Auto.test = Auto[test, ]
+mpg01.test = mpg01[test]
+
+# LDA
+library(MASS)
+lda.fit = lda(mpg01 ~ cylinders + weight + displacement + horsepower, data = Auto, 
+              subset = train)
+lda.pred = predict(lda.fit, Auto.test)
+# 12.6% test error rate.
+mean(lda.pred$class != mpg01.test)
+
+# QDA
+qda.fit = qda(mpg01 ~ cylinders + weight + displacement + horsepower, data = Auto, 
+              subset = train)
+qda.pred = predict(qda.fit, Auto.test)
+mean(qda.pred$class != mpg01.test)
+
+# Logistic regression
+# subset is train
+glm.fit = glm(mpg01 ~ cylinders + weight + displacement + horsepower, data = Auto, 
+              family = binomial, subset = train)
+# type is response for logisitc regression
+glm.probs = predict(glm.fit, Auto.test, type = "response")
+glm.pred = rep(0, length(glm.probs))
+glm.pred[glm.probs > 0.5] = 1
+mean(glm.pred != mpg01.test)
+
+# KNN
+library(class)
+train.X = cbind(cylinders, weight, displacement, horsepower)[train, ]
+test.X = cbind(cylinders, weight, displacement, horsepower)[test, ]
+train.mpg01 = mpg01[train]
+set.seed(1)
+knn.pred = knn(train.X, test.X, train.mpg01, k = 1)
+mean(knn.pred != mpg01.test)
+
+# remove all objects at once
+rm(list=ls())
+
+# Q12
+Power = function() {
+    2^3
+}
+print(Power())
+
+Power2 = function(x, a) {
+    x^a
+}
+Power2(3, 8)
+
+Power2(10, 3)
+
+Power3 = function(x, a) {
+    result = x^a
+    return(result)
+}
+
+x = 1:10
+plot(x, Power3(x, 2), log = "xy", ylab = "Log of y = x^2", xlab = "Log of x", 
+     main = "Log of x^2 versus Log of x")
