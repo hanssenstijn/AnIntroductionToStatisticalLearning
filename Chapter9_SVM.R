42a676a0c7b059d4764a950e83371c876735447a
Stijn <srs.hanssen@student.maastrichtuniversity.nl>
2019-01-28 20:35:25 +0100

    update script chapter 9
    - function for ROC curve
    - SVM on multiple classes, using "radial"
    Excercises
    - Q7 if else function, on if higher or lower is than a value > binarize
    - tune function > using different cost values
    - using best tune cost value > var$best.parameters$cost

--- Chapter9_SVM.R
+++ Chapter9_SVM.R
@@ -86,3 +86,109 @@ summary(tune.out)
 table(true=dat[-train ,"y"], pred=predict(tune.out$best.model ,
                                            newx=dat[-train ,]))
 
+
+# ROC curves
+library(ROCR)
+# Function to plot an ROC curve
+#given a vector containing a numerical score for each observation, pred, and
+#a vector containing the class label for each observation, truth.
+rocplot =function (pred , truth , ...){
+    predob = prediction (pred , truth )
+    perf = performance (predob , "tpr", "fpr")
+    plot(perf ,...)}
+# if the fitted value exceeds zero then the observation
+# is assigned to one class, and if it is less than zero than it is assigned to the other.
+# In order to obtain the fitted values for a given SVM model fit, we
+# use decision.values=TRUE when fitting svm().
+svmfit.opt=svm(y~., data=dat[train ,], kernel ="radial",
+                gamma =2, cost=1, decision.values =T)
+fitted =attributes (predict (svmfit.opt ,dat[train ,], decision.values =TRUE))$decision.values
+par(mfrow =c(1,2))
+rocplot(fitted ,dat [train ,"y"], main=" Training Data")
+
+# SVM with multiple classes
+set.seed(1)
+x=rbind(x, matrix (rnorm (50*2) , ncol =2))
+y=c(y, rep (0 ,50) )
+x[y==0 ,2]= x[y==0 ,2]+2
+dat=data.frame(x=x, y=as.factor (y))
+par(mfrow =c(1,1))
+plot(x,col =(y+1))
+svmfit =svm(y~., data=dat , kernel ="radial", cost =10, gamma =1)
+plot(svmfit , dat)
+
+# application to gene expression data
+library(ISLR)
+names(Khan)
+dim( Khan$xtrain)
+dim( Khan$xtest)
+table(Khan$ytrain)
+table(Khan$ytest)
+# training data
+dat=data.frame(x=Khan$xtrain , y=as.factor(Khan$ytrain))
+out=svm(y~., data=dat , kernel ="linear",cost =10)
+summary(out)
+table(out$fitted , dat$y)
+
+# test data
+dat.te=data.frame(x=Khan$xtest , y=as.factor(Khan$ytest))
+pred.te=predict(out , newdata =dat.te)
+table(pred.te , dat.te$y)
+
+# Excercises
+# Q3.
+x1 = c(3, 2, 4, 1, 2, 4, 4)
+x2 = c(4, 2, 4, 4, 1, 3, 1)
+colors = c("red", "red", "red", "red", "blue", "blue", "blue")
+plot(x1, x2, col = colors, xlim = c(0, 5), ylim = c(0, 5))
+abline(-0.5, 1)
+abline(-1, 1, lty = 2)
+abline(0, 1, lty = 2)
+arrows(2, 1, 2, 1.5)
+arrows(2, 2, 2, 1.5)
+arrows(4, 4, 4, 3.5)
+arrows(4, 3, 4, 3.5)
+
+#Q7
+library(ISLR)
+gas.med = median(Auto$mpg)
+# if else higher than median
+new.var = ifelse(Auto$mpg > gas.med, 1, 0)
+Auto$mpglevel = as.factor(new.var)
+
+library(e1071)
+# using various cost values
+set.seed(3255)
+tune.out = tune(svm, mpglevel ~ ., data = Auto, kernel = "linear", ranges = list(cost = c(0.01, 
+                                                                                  0.1, 1, 5, 10, 100)))
+# We see that cross-validation error is minimized for cost=1.
+summary(tune.out)
+
+# Q8
+library(ISLR)
+set.seed(9004)
+# create training and test set
+train = sample(dim(OJ)[1], 800)
+OJ.train = OJ[train, ]
+OJ.test = OJ[-train, ]
+
+svm.linear = svm(Purchase ~ ., kernel = "linear", data = OJ.train, cost = 0.01)
+summary(svm.linear)
+# predict training error
+train.pred = predict(svm.linear, OJ.train)
+table(OJ.train$Purchase, train.pred)
+# test error
+test.pred = predict(svm.linear, OJ.test)
+table(OJ.test$Purchase, test.pred)
+
+# tune function to select an optimal cost
+set.seed(1554)
+tune.out = tune(svm, Purchase ~ ., data = OJ.train, kernel = "linear", ranges = list(cost = 10^seq(-2, 
+                                                                                                   1, by = 0.25)))
+summary(tune.out)
+
+# using best tune value
+svm.linear = svm(Purchase ~ ., kernel = "linear", data = OJ.train, cost = tune.out$best.parameters$cost)
+train.pred = predict(svm.linear, OJ.train)
+table(OJ.train$Purchase, train.pred)
+
